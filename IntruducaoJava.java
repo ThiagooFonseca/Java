956c09f192f167780af008134efa62c9c5aba3a5
Thiago Fonseca <thiagofonsecasantanasouza@gmail.com>
2022-05-06 16:56:36 -0300

    Introdução Java

---
+++ IntruducaoJava.java
@@ -0,0 +1,44 @@
+package IntroduçãoGen;
+
+import java.util.Scanner;
+
+public class IntruducaoJava {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		int idade ;
+		
+			System.out.println("Digite Sua idade: ");
+			idade=ler.nextInt();
+			
+			if(idade>10 && idade<=14)
+				System.out.println("Criança");
+			else if(idade>15 && idade<=17)
+				System.out.println("Adolescente");
+			else 
+				System.out.println("Adulto");		
+				
+			
+			
+				
+						
+					
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
